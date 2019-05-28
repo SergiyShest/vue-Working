b1d320f1337034acb168733fec8b8349fc9abdf2
Sergiy <Shest_akow@rambler.ru>
2019-05-28 04:53:58 +0300

    Синхронизация с разработкой фильтра

--- datagrid-mvc5/js/code2.js
+++ datagrid-mvc5/js/code2.js
@@ -1,56 +1,9 @@
-﻿function FilterField(dataGridColumn, input, checkBox, comparator = '=') {
-    this.column = dataGridColumn;
-    this.DataField = dataGridColumn.dataField;
-    this.DataType = dataGridColumn.dataType;
-    this.Comparator = comparator;
-    this.Value = null;
-    this.Input = input;
-    this.CheckBox = checkBox;
-    this.ApplayFilter = ApplayFilter;
-
-    function ApplayFilter(collectiveFilter) {
-        if (checkBox.checked) {
-            filter = new Array();
-            var val = this.Input.value;
-            switch (this.DataType) {
-                case 'number': {
-                    if (val.includes(',')) {
-                        filter.push(filterCommaSplittedCodition(this.DataField, val));
-                    }
-                    else { filter.push([this.DataField, '=', val]); }
-                } break;
-                case 'string': { filter.push([this.DataField, 'contains', val]); }
-            }
-            collectiveFilter.push(filter);
-        }
-    }
-
-    function filterCommaSplittedCodition(dataField, inpVal) {
-        inpVal = inpVal.replace(/\s/g, '').replace(/,+/g, ',');//remove repeated commas and spaces
-        var arr = inpVal.split(',');
-        filter = new Array();
-        if (arr.length == 1 && arr[0] == '') { } else {
-            for (f = 0, i = 0; i < arr.length; i++ , f++) {
-                filter[f] = [dataField, "=", arr[i]];
-                if (arr.length - 1 > i) {
-                    f++;
-                    filter[f] = 'or'
-                }
-            }
-        }
-        return filter;
-    }
-}
-
-
-//установка источника данных
+﻿//установка источника данных
 function setDataSource() {
     const dataSource = DevExpress.data.AspNet.createStore({
         key: "OrderID",
-        loadUrl: "../Orders/Get",
-        insertUrl: "../Orders/Post",
-        updateUrl: "../Orders/Put",
-        deleteUrl: "../Orders/Delete"
+        loadUrl: "../Orders/Get"
+ 
     });
 
     var grid = $("#grid").dxDataGrid("instance");
@@ -67,38 +20,32 @@ function copyToClipboard(str) {
     document.body.removeChild(el);
 };
 
-var ini = false;
+var isinit = false;
 //write time loading data
 function contentReady() {
-    if (ini) {
-        console.log(" time loading data");
-        console.timeEnd("x");
-        return;
-    } else {
-        ini = true;
-        setDataSource();
-    }
-
-
-
+    if (!isinit) {
+        isinit = true;  setDataSource();}
+    // dataGrid = $('#grid').dxDataGrid('instance');
+    //   console.log(" time loading data");
+    //   console.timeEnd("x");
 }
 
-//получение строки фильтров
+ //получение строки фильтров
 function processFilter(dataGridInstance, filter) {
-    if ($.isArray(filter)) {
-        if ($.isFunction(filter[0])) {
-            filter[0] = getColumnFieldName(dataGridInstance, filter[0]);
-        }
-        else {
-            for (var i = 0; i < filter.length; i++) {
-                processFilter(dataGridInstance, filter[i]);
+        if ($.isArray(filter)) {
+            if ($.isFunction(filter[0])) {
+                filter[0] = getColumnFieldName(dataGridInstance, filter[0]);
+            }
+            else {
+                for (var i = 0; i < filter.length; i++) {
+                    processFilter(dataGridInstance, filter[i]);
+                }
             }
         }
     }
-}
 
 
-var statuses = ["All", 'Switzerland', 'Belgium', 'Brazil', 'France', 'Brazil', 'Germany',];
+
 //добавление пункта меню копировать
 function contextMenuPreparing(e) {
     if (e.target == 'header') {
@@ -134,20 +81,7 @@ $(function () {
         }
     }
 
-    $("#testFilter").dxButton({
-        text: "проверка фильтра",
-        onClick: function (e) {
-            var grid = $("#grid").dxDataGrid("instance"),
-                filter = grid.getCombinedFilter();
-            alert(filter);
-            processFilter(grid, filter);
-            alert(filter);
-        }
-    }).dxButton("instance");
-    $("#test").dxButton({
-        text: "data",
-        onClick: setDataSource
-    }).dxButton("instance");
+
 
 
     //поиск имени конолки по фильтру
@@ -169,179 +103,35 @@ $(function () {
     }
 
 
-    $("#grid").dxDataGrid({
-        height: 500,
-        remoteOperations: { paging: true, filtering: true, sorting: true, grouping: true, summary: true, groupPaging: true },
-        //    dataSource:"../Orders/Get",
-        //    dataSource: DevExpress.data.AspNet.createStore({
-        //    key: "OrderID",
-        //    loadUrl: "../Orders/Get",
-        //    insertUrl: "../Orders/Post",
-        //    updateUrl: "../Orders/Put",
-        //    deleteUrl: "../Orders/Delete"
-        //}),
-        onContentReady: contentReady,
-        paging: {
-            pageSize: 30
-        },
-        onContextMenuPreparing: contextMenuPreparing,
-        focusedRowEnabled: true,
-        rowAlternationEnabled: true,
-        focusedRowKey: 3,
-        // filterValue: [["OrderID", ">", "400"], "and", ["OrderID", '<',"100000"]],
-        onInitNewRow: function (e) {
-            e.data = {
-                OrderDate: new Date()
-            };
-        },
-        columnAutoWidth: true,
-        filterRow: {
-            visible: true,
-            applyFilter: "auto"
-        },
+   $("#grid").dxDataGrid({
+            height: 500,
+            remoteOperations: { paging: true, filtering: true, sorting: true, grouping: true, summary: true, groupPaging: true },
+            keyExpr: "OrderID",
+            onContentReady:contentReady,
+            
+            paging: {
+                pageSize: 30
+            },
 
-        headerFilter: { visible: true },
-        filterPanel: { visible: true },
-        selection: {
-            mode: "multiple",
-            allowSelectAll: true
-        },
-        columns: [
-            {
-                dataField: "OrderID",
-                formItem: {
-                    visible: true
-                },
-                headerCellTemplate: $('<i style="color: black">ID</i>')
+            onContextMenuPreparing: contextMenuPreparing,
+            focusedRowEnabled: true,
+            rowAlternationEnabled: true,
+            focusedRowKey: 3,
+  
+             columnAutoWidth: true,
+            filterRow: {
+                visible: true,
+                applyFilter: "auto"
             },
-            {
-                caption: "Customer",
-                calculateDisplayValue: "CustomerName",
-                dataField: "CustomerID",
-                lookup: {
-                    valueExpr: "CustomerID",
-                    displayExpr: "ContactName",
-                    dataSource: {
-                        paginate: true,
-                        store: DevExpress.data.AspNet.createStore({
-                            key: "CustomerID",
-                            loadUrl: "Customers/Get"
-                        })
-                    }
-                }
+
+            headerFilter: { visible: true },
+            filterPanel: { visible: true },
+            selection: {
+                mode: "multiple",
+                allowSelectAll: true
             },
-            {
-                caption: "Employee",
-                calculateDisplayValue: "EmployeeName",
-                dataField: "EmployeeID",
-                lookup: {
-                    valueExpr: "EmployeeID",
-                    displayExpr: "FullName",
-                    dataSource: {
-                        paginate: true,
-                        store: DevExpress.data.AspNet.createStore({
-                            key: "EmployeeID",
-                            loadUrl: "Employees/Get"
-                        })
-                    }
-                }
-            },
-            { caption: "дата ", dataField: "OrderDate", dataType: "date" },
-            { dataField: "RequiredDate", dataType: "date" },
-            { dataField: "ShippedDate", dataType: "date" },
-            {
-                dataField: "ShipVia",
-                calculateDisplayValue: "ShipViaName",
-                lookup: {
-                    valueExpr: "ShipperID",
-                    displayExpr: "CompanyName",
-                    dataSource: {
-                        paginate: true,
-                        store: DevExpress.data.AspNet.createStore({
-                            key: "ShipperID",
-                            loadUrl: "Shippers/Get"
-                        })
-                    }
-                }
-            },
-            "Freight",
-            "ShipName",
-            "ShipAddress",
-            "ShipCity",
-            "ShipCountry"
-        ]
+            columns:colunmDescription, 
+        });
+
+  
     });
-
-    function masterDetailTemplate(container, options) {
-        $("<div>").addClass("grid").appendTo(container).dxDataGrid({
-            remoteOperations: true,
-            dataSource: {
-                filter: ["OrderID", "=", options.key],
-                store: DevExpress.data.AspNet.createStore({
-                    key: ["OrderID", "ProductID"],
-                    loadUrl: "OrderDetails/Get",
-                    insertUrl: "OrderDetails/Post",
-                    updateUrl: "OrderDetails/Put",
-                    deleteUrl: "OrderDetails/Delete",
-                })
-            },
-            showBorders: true,
-            editing: {
-                allowUpdating: true,
-                allowAdding: true,
-                allowDeleting: true
-            },
-            onInitNewRow: function (e) {
-                e.data = {
-                    OrderID: options.key,
-                    Quantity: 1,
-                    Discount: 0
-                }
-            },
-            onEditorPreparing: function (e) {
-                if (e.dataField === "ProductID") {
-                    var dataGrid = e.component;
-                    var valueChanged = e.editorOptions.onValueChanged;
-                    e.editorOptions.onValueChanged = function (args) {
-                        valueChanged.apply(this, arguments);
-
-                        var product = args.component.getDataSource().items().filter(function (data) { return data.ProductID === args.value })[0];
-
-                        if (product) {
-                            dataGrid.cellValue(e.row.rowIndex, "UnitPrice", product.UnitPrice);
-                        }
-                    }
-                }
-            },
-            summary: {
-                totalItems: [
-                    { column: "Total", summaryType: "sum", displayFormat: "Total: {0}", valueFormat: { type: "currency", precision: 2 } }
-                ]
-            },
-            columns: [
-                {
-                    dataField: "ProductID",
-                    caption: "Product",
-                    calculateDisplayValue: "ProductName",
-                    lookup: {
-                        valueExpr: "ProductID",
-                        displayExpr: "ProductName",
-                        dataSource: {
-                            paginate: true,
-                            store: DevExpress.data.AspNet.createStore({
-                                key: "ProductID",
-                                loadUrl: "Products/Get"
-                            })
-                        }
-                    }
-                },
-                { dataField: "UnitPrice", format: { type: "currency", precision: 2 }, allowEditing: false },
-                "Quantity",
-                "Discount",
-                { dataField: "Total", format: { type: "currency", precision: 2 }, allowEditing: false, calculateCellValue: function (data) { return data.UnitPrice ? data.UnitPrice * data.Quantity * (1 - data.Discount) : null } }
-            ]
-        })
-    }
-
-
-});
