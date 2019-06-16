50450713c83f49a01ae81b6a5734ff7f5b4a49ff
Sergiy <Shest_akow@rambler.ru>
2019-06-16 10:07:17 +0300

    Добавлен компонент ErrorAborder

---
+++ datagrid-mvc5/Scripts/components/vueErrorAborder.js
@@ -0,0 +1,18 @@
+﻿Vue.component('error-aborder',
+    {
+        props: {
+            error: String
+        },
+        data: function () {
+            return {
+                count: 0
+            }
+        },
+        template:
+            '<div class="tooltip" >' +
+                '<div v-bind:class="{error:!error==\'\' }" >' +
+                '<slot>test</slot>' +
+                '</div>' +
+                '<p  class="tooltiptext"  v-if="!error==\'\'" >{{error}}</p>' +
+                '</div>'
+    });
