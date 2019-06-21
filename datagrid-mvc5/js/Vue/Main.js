9ffc0c0347d3a84b135f2d09cdee1e1bba3ea3b8
Sergiy <Shest_akow@rambler.ru>
2019-06-21 19:26:40 +0300

    Remove base functional in Mixine

---
+++ datagrid-mvc5/js/Vue/Main.js
@@ -0,0 +1,108 @@
+﻿new Vue({
+    el: "#app",
+    mixins: [baseMixin],
+    data: {
+        id: ID,
+        selected: '',
+        order: {
+            OrderID: 0,
+            CustomerID: 0,
+            EmployeeID: 0,
+            OrderDate: "",
+            RequiredDate: "",
+            ShippedDate: "",
+            ShipVia: 0,
+            Freight: 0,
+            ShipName: "",
+            ShipAddress: "",
+            ShipCity: "",
+            ShipRegion: "",
+            ShipPostalCode: "",
+            ShipCountry: ""
+        },
+        oldOrder: {
+            ShipCountry: ""
+        },
+        errors: {
+            OrderID: null,
+            CustomerID: null,
+            EmployeeID: null,
+            OrderDate: null,
+            RequiredDate: null,
+            ShippedDate: null,
+            ShipVia: null,
+            Freight: null,
+            ShipName: null,
+            ShipAddress: null,
+            ShipCity: null,
+            ShipRegion: null,
+            ShipPostalCode: null,
+            ShipCountry: null
+        },
+        IsChanged: false,
+        AvaialbeCustomers: [],
+        AvaialbeCitys: [],
+        AvaialbeRegions: [],
+        AvaialbeCountrys: []
+    },
+    computed:
+    {
+        hasError: function () {
+            for (var err in this.errors) {
+                var error = this.errors[err];
+                if (error !== '' || null) return true;
+            }
+            return false;
+        }
+    },
+    methods: {
+        fetchOrder() {
+            var path = "../Orders/GetById?Id=" + this.id;
+            this.fetchJson(path, json => this.order = json);
+        },
+
+        fetchCityList() {
+            //Avaiable city depended on  country
+            var country = this.order.ShipCountry;
+            if (country == null || country === "") {
+                country = '';
+            }
+            var path = "../Orders/AvaiableCityList?country=" + country;
+            this.fetchJson(path, json => { this.AvaialbeCitys = json; });
+        },
+
+        fetchCountrys() {
+            var path = "../Orders/AvaiableCountrys";
+            this.fetchJson(path, jsonResult => { this.AvaialbeCountrys = jsonResult; });
+        },
+
+        fetchAvaialbeCustomers() {
+            var path = "../Orders/AvaialbeCustomers";
+            this.fetchJson(path, jsonResult => { this.AvaialbeCustomers = jsonResult; });
+        },
+
+        SaveOldValue() {
+            this.oldOrder.ShipCountry = this.order.ShipCountry;
+        }
+    },
+    watch: {
+        order: {
+            handler: function (after) {
+                this.IsChanged = true;
+                if (this.oldOrder.ShipCountry !== after.ShipCountry) {
+                    this.fetchCityList();
+                }
+                this.SaveOldValue();
+                this.Validate();
+            },
+            deep: true
+        }
+    },
+    mounted: function () {
+        this.ActionPath = "../Orders/";
+        this.fetchOrder();
+        this.SaveOldValue();
+        this.fetchCountrys();
+        this.fetchAvaialbeCustomers();
+    }
+});
---
+++ datagrid-mvc5/js/Vue/components/BaseMIxin.js
@@ -0,0 +1,54 @@
+﻿var baseMixin = {
+    data: function () {
+        return {
+            abortController: new AbortController(),
+            validationCounter: 0,
+            ActionPath: ""
+        }
+    },
+    methods: {
+        fetchJson(path, collback, signal) {
+            try {
+                fetch(path, { mode: 'cors', signal: signal })
+                    .then(response => response.json())
+                    .then(function (json) {
+                        collback(json);
+                    }
+                    );
+            } catch (ex) {
+                alert(ex);
+            }
+        },
+        //Валидация с задержкой и прерыванием предыдущей валидации, если последущая валидация не отработала
+        Validate() {
+            this.validationCounter++;//увеличиваю счетчик валидации, что бы предыдущая валидация не отработала
+            var counter = this.validationCounter;//создаю копию переменнной
+            var delayedValidateRef = this.delayedValidate;//так как я не могу использовать this локальной фнукции создавать ссылку на функцию
+            setTimeout(function () { delayedValidateRef(counter); }, 500);//вызываю валидацию с задержкой ()
+
+
+        },
+        delayedValidate(сounter) {
+            if (this.validationCounter !== сounter)//если счетчик валидаций изменился за время задержки валидации, то эту валидацию надо прервать
+            {
+                return;
+            }
+            console.log("Validate");
+            this.abortController.abort();//прервать предыдущую валидацию если она успела начаться
+            this.abortController = new AbortController();//создаю новый контроллер валидации
+            this.Action("Validate", this.abortController.signal);//перадаю сигнал позволяющий "убить" ПРЕДЫДУЩИЙ фетч
+        },
+        Save() {
+            this.abortController.abort();//прервать предыдущую валидацию
+            this.Action("Save");
+        },
+        Action(action, signal) {
+            var myJSON = JSON.stringify(this.order);
+            var path = this.ActionPath + action + "?id=" + this.id + "&json=" + myJSON;
+            this.fetchJson(path, jsonResult => {
+                this.errors = jsonResult.Errors;
+                this.IsChanged = jsonResult.IsChanged;
+            }, signal);
+        },
+    }
+}
