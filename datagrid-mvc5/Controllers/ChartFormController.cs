83cbf85e99c462caa6e684c246ebcd106a9a361e
Sergiy <Shest_akow@rambler.ru>
2019-09-30 05:07:53 +0300

    last

--- datagrid-mvc5/Controllers/ChartFormController.cs
+++ datagrid-mvc5/Controllers/ChartFormController.cs
@@ -3,7 +3,9 @@ using DevExtreme.AspNet.Data;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 
@@ -23,6 +25,28 @@ namespace datagrid_mvc5.Controllers
             sessionStor.sessionId = seessionID;
             return View("ChatForm");
         }
+        [HttpPost]
+        public ActionResult uploadFile()
+        {
+
+            foreach (string file in base.Request.Files)
+            {
+                // file.InputStream.CopyTo(ms);
+                // byte[] array = ms.GetBuffer();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    if (Request.Files[file].FileName != "")
+                    {
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/";
+                        string filename = Path.GetFileName(Request.Files[file].FileName);
+                        Request.Files[file].SaveAs(Path.Combine(path, filename));
+                    }
+                }
+            }
+            return RedirectToAction("upload");
+        }
+
     }
     public static class sessionStor
     {
